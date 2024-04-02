b1be82c7fbdd612f526ec819ec5684d4fb7c11cc
tw30006 <original.heart.2016@gmail.com>
2024-04-02 12:13:53 +0800

    21題

---
+++ 21.js
@@ -0,0 +1,33 @@
+// 編號：CANDY-021
+// 程式語言：JavaScript
+// 題目：實作 Stack 資料結構
+
+class Stack {
+  // 實作寫在這裡
+  constructor() {
+    this.arr = [];
+  }
+  push(element) {
+    if (element !== undefined) {
+      return this.arr.push(element);
+    }
+  }
+  pop() {
+    return this.arr.pop();
+  }
+  get size() {
+    return this.arr.length;
+  }
+}
+
+const stack = new Stack();
+stack.push(123);
+stack.push(456);
+stack.push();
+console.log(stack.size); // 印出 2
+
+const item = stack.pop(); // 取出元素
+console.log(item); // 印出 456
+
+stack.pop(); // 繼續取出元素
+console.log(stack.size); // 印出 0
