0a6e60357b4da7ce6a38d8c7187bef95d414eb5e
tw30006 <original.heart.2016@gmail.com>
2024-04-02 12:14:35 +0800

    22題

---
+++ 22.js
@@ -0,0 +1,33 @@
+// 編號：CANDY-022
+// 程式語言：JavaScript
+// 題目：實作 Queue 資料結構
+
+class Queue {
+  // 實作寫在這裡
+  constructor() {
+    this.arr = [];
+  }
+  enqueue(element) {
+    if (element !== undefined) {
+      return this.arr.push(element);
+    }
+  }
+  dequeue() {
+    return this.arr.shift();
+  }
+  get length() {
+    return this.arr.length;
+  }
+}
+
+const queue = new Queue();
+queue.enqueue(123);
+queue.enqueue(456);
+queue.enqueue();
+console.log(queue.length); // 印出 2
+
+const item = queue.dequeue(); // 取出元素
+console.log(item); // 印出 123
+
+queue.dequeue(); // 繼續取出元素
+console.log(queue.length); // 印出 0
